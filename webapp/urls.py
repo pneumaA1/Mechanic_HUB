144ac11aeb88f7ffa7b11e71dbf03199621a1ecd
pneumA1 <mikhalovskiys@gmail.com>
2023-03-20 23:22:21 +0300

    Optimized templates. Routes are configured.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,34 @@
 from django.urls import path
 
 from webapp.views import *
+
+app_name = 'webapp'
+
 urlpatterns = [
     path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('account/', account, name='account'),
+    path('blog-grig/', blog_grid, name='blog-grid'),
+    path('blog-single/', blog_single, name='blog-single'),
+    path('blog-sb/', blog_with_sidebar, name='blog-sb'),
+    path('checkout/', checkout, name='checkout'),
+    path('contact/', contact, name='contact'),
+    path('faq/', faq, name='faq'),
+    path('gallery/', gallery, name='gallery'),
+    path('gallety-single/', gallery_single, name='gallery-single'),
+    path('pricing/', pricing, name='pricing'),
+    path('services/', services, name='services'),
+    path('shop/', shop, name='shop'),
+    path('shop-single/', shop_single, name='shop-single'),
+    path('shopping-cart/', shopping_cart, name='shopping-cart'),
+    path('team/', team, name='team'),
+    path('testimonials/', testimonials, name='testimonials'),
+    path('wheel-works/', wheel_works, name='wheel-works'),
+    path('painting_works', painting_works, name='painting-works'),
+    path('air-conditioner/', air_conditioner, name='air-conditioner'),
+    path('water-service/', water_service, name='water-service'),
+    path('engine-works/', engine_works, name='engine-works'),
+    path('oil-filters/', oil_filter, name='oil-filters'),
+    path('brake-repairs', brake_repairs, name='brake-repairs'),
+    path('belts-hoses/', belts_hoses, name='belts-hoses')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,76 @@
 from django.shortcuts import render
-
-# Create your views here.
 def index(request):
     return render(request, 'webapp/index-2.html')
+
+def about(request):
+    return render(request, 'webapp/about.html')
+
+def account(request):
+    return render(request, 'webapp/account.html')
+
+def blog_grid(request):
+    return render(request, 'webapp/blog-grid.html')
+
+def blog_single(request):
+    return render(request, 'webapp/blog-single.html')
+
+def blog_with_sidebar(request):
+    return render(request, 'webapp/blog-with-sidebar.html')
+
+def checkout(request):
+    return render(request, 'webapp/checkout.html')
+
+def contact(request):
+    return render(request, 'webapp/contact.html')
+
+def faq(request):
+    return render(request, 'webapp/faq.html')
+
+def gallery(request):
+    return render(request, 'webapp/gallery.html')
+
+def gallery_single(request):
+    return render(request, 'webapp/gallery-single.html')
+
+def pricing(request):
+    return render(request, 'webapp/pricing.html')
+
+def services(request):
+    return render(request, 'webapp/services.html')
+
+def shop(request):
+    return render(request, 'webapp/shop.html')
+def shop_single(request):
+    return render(request, 'webapp/shop-single.html')
+
+def shopping_cart(request):
+    return render(request, 'webapp/shopping-cart.html')
+
+def team(request):
+    return render(request, 'webapp/team.html')
+def testimonials(request):
+    return render(request, 'webapp/testimonials.html')
+
+def wheel_works(request):
+    return render(request, 'webapp/ser-single1-wheel-works.html')
+
+def painting_works(request):
+    return render(request, 'webapp/ser-single2-painting-works.html')
+
+def air_conditioner(request):
+    return render(request, 'webapp/ser-single3-air-conditioner.html')
+
+def water_service(request):
+    return render(request, 'webapp/ser-single4-water-service.html')
+
+def engine_works(request):
+    return render(request, 'webapp/ser-single5-engine-works.html')
+
+def oil_filter(request):
+    return render(request, 'webapp/ser-single6-lube-oil-filters.html')
+
+def brake_repairs(request):
+    return render(request, 'webapp/ser-single7-brake-repairs.html')
+
+def belts_hoses(request):
+    return render(request, 'webapp/ser-single8-belts-hoses.html')
